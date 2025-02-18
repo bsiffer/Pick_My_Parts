f3a165863f71c7ce97d0ceda010889db395dac6e
Devin L. Hackman <139880411+devinlanehackman@users.noreply.github.com>
2025-02-18 09:39:11 -0500

    Updating cpu with extra variables for compatibility

--- src/models/cpu.py
+++ src/models/cpu.py
@@ -12,12 +12,24 @@ class cpu(Part):
         price: float,
         architecture: str,
         clock_speed: float,
+        ddr4_compatibility: bool,
+        ddr5_compatibility: bool,
+        socket_type: str,
+        wattage_compatibility: float,
+        bios_compatibility: str,
+        chipset_compatibility: str,
         # Compatibility to be determined later
     ):
         # Call super class constructor
         super().__init__(manufacturer, name, sku, price)
         self.__architecture = architecture
         self.__clock_speed = clock_speed
+        self.__ddr4_compatibility = ddr4_compatibility
+        self.__ddr5_compatibility = ddr5_compatibility
+        self.__socket_type = socket_type
+        self.__wattage_compatibility = wattage_compatibility
+        self.__bios_compatibility = bios_compatibility
+        self.__chipset_compatibility = chipset_compatibility
 
     # Getters and Setters for private variables __architecture and __clock_speed
     def get_architecture(self):
@@ -26,11 +38,47 @@ class cpu(Part):
     def get_clock_speed(self):
         return self.__clock_speed
 
+    def get_ddr4_compatibility(self):
+        return self.__ddr4_compatibility
+
+    def get_ddr5_compatibility(self):
+        return self.__ddr5_compatibility
+
+    def get_socket_type(self):
+        return self.__socket_type
+
+    def get_wattage_compatibility(self):
+        return self.__wattage_compatibility
+
+    def get_bios_compatibility(self):
+        return self.__bios_compatibility
+
+    def get_chipset_compatibility(self):
+        return self.__chipset_compatibility
+
     def set_architecture(self, architecture):
         self.__architecture = architecture
 
     def set_clock_speed(self, clock_speed):
         self.__clock_speed = clock_speed
+
+    def set_ddr4_compatibility(self, ddr4_compatibility):
+        self.__ddr4_compatibility = ddr4_compatibility
+
+    def set_ddr5_compatibility(self, ddr5_compatibility):
+        self.__ddr5_compatibility = ddr5_compatibility
+
+    def set_socket_type(self, socket_type):
+        self.__socket_type = socket_type
+
+    def set_wattage_compatibility(self, wattage_compatibility):
+        self.__wattage_compatibility = wattage_compatibility
+
+    def set_bios_compatibility(self, bios_compatibility):
+        self.__bios_compatibility = bios_compatibility
+
+    def set_chipset_compatibility(self, chipset_compatibility):
+        self.__chipset_compatibility = chipset_compatibility
 
     # Method to display part and CPU details.
     def display_info(self):
@@ -38,3 +86,6 @@ class cpu(Part):
             super().display_info()
             + f"Architecture: {self.__architecture}\nClock Speed: {self.__clock_speed}\n"
         )
+
+    def Check_compatibility():
+        pass  # To be implemented later
