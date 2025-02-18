ae508e05e5f781836b10382ab89b1d7e73de5872
Devin L. Hackman <139880411+devinlanehackman@users.noreply.github.com>
2025-02-18 09:40:06 -0500

    Adding testing to accommodate new variables/methods

--- src/tests/test_cpu.py
+++ src/tests/test_cpu.py
@@ -9,6 +9,12 @@ class TestCPU(unittest.TestCase):
     __price = 339.99
     __architecture = "Zen 5"
     __clock_speed = 4.7
+    __ddr4_compatibility = True
+    __ddr5_compatibility = False
+    __socket_type = "AM5"
+    __wattage_compatibility = 105
+    __bios_compatibility = "Yes"
+    __chipset_compatibility = "Yes"
 
     def setUp(self):
         self.__cpu = cpu(
@@ -18,6 +24,12 @@ class TestCPU(unittest.TestCase):
             self.__price,
             self.__architecture,
             self.__clock_speed,
+            self.__ddr4_compatibility,
+            self.__ddr5_compatibility,
+            self.__socket_type,
+            self.__wattage_compatibility,
+            self.__bios_compatibility,
+            self.__chipset_compatibility,
         )
 
     def test_get_architecture(self):
@@ -26,6 +38,28 @@ class TestCPU(unittest.TestCase):
     def test_get_clock_speed(self):
         self.assertEqual(self.__cpu.get_clock_speed(), self.__clock_speed)
 
+    def test_get_ddr4_compatibility(self):
+        self.assertEqual(self.__cpu.get_ddr4_compatibility(), self.__ddr4_compatibility)
+
+    def test_get_ddr5_compatibility(self):
+        self.assertEqual(self.__cpu.get_ddr5_compatibility(), self.__ddr5_compatibility)
+
+    def test_get_socket_type(self):
+        self.assertEqual(self.__cpu.get_socket_type(), self.__socket_type)
+
+    def test_get_wattage_compatibility(self):
+        self.assertEqual(
+            self.__cpu.get_wattage_compatibility(), self.__wattage_compatibility
+        )
+
+    def test_get_bios_compatibility(self):
+        self.assertEqual(self.__cpu.get_bios_compatibility(), self.__bios_compatibility)
+
+    def test_get_chipset_compatibility(self):
+        self.assertEqual(
+            self.__cpu.get_chipset_compatibility(), self.__chipset_compatibility
+        )
+
     def test_set_architecture(self):
         self.__cpu.set_architecture("Zen 3")
         self.assertEqual(self.__cpu.get_architecture(), "Zen 3")
@@ -33,6 +67,30 @@ class TestCPU(unittest.TestCase):
     def test_set_clock_speed(self):
         self.__cpu.set_clock_speed(4.2)
         self.assertEqual(self.__cpu.get_clock_speed(), 4.2)
+
+    def test_set_ddr4_compatibility(self):
+        self.__cpu.set_ddr4_compatibility(False)
+        self.assertEqual(self.__cpu.get_ddr4_compatibility(), False)
+
+    def test_set_ddr5_compatibility(self):
+        self.__cpu.set_ddr5_compatibility(True)
+        self.assertEqual(self.__cpu.get_ddr5_compatibility(), True)
+
+    def test_set_socket_type(self):
+        self.__cpu.set_socket_type("AM4")
+        self.assertEqual(self.__cpu.get_socket_type(), "AM4")
+
+    def test_set_wattage_compatibility(self):
+        self.__cpu.set_wattage_compatibility(95)
+        self.assertEqual(self.__cpu.get_wattage_compatibility(), 95)
+
+    def test_set_bios_compatibility(self):
+        self.__cpu.set_bios_compatibility("No")
+        self.assertEqual(self.__cpu.get_bios_compatibility(), "No")
+
+    def test_set_chipset_compatibility(self):
+        self.__cpu.set_chipset_compatibility("No")
+        self.assertEqual(self.__cpu.get_chipset_compatibility(), "No")
 
     def test_display_info(self):
         expected_output = (
