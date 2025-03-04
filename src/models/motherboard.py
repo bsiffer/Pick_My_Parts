d41cc6dcc912d94fbcf69dc700d5ecbe37aa48e8
MichaelStainback5 <119711642+MichaelStainback5@users.noreply.github.com>
2025-03-03 19:07:58 -0500

    Update motherboard.py
    included compatibilty methods

--- src/models/motherboard.py
+++ src/models/motherboard.py
@@ -1,5 +1,6 @@
 """ Motherboard class inheriting from part.py """
 from src.models.part import Part
+from src.utils.parts_compatibility_manager import check_storage_motherboard
 
 class Motherboard(Part):
     def __init__(self, manufacturer, name, sku, price, architecture, standard_size, ram_slots, compatibility):
@@ -8,6 +9,14 @@ class Motherboard(Part):
         self.standard_size = standard_size  # e.g., ATX, Micro-ATX, Mini-ITX
         self.ram_slots = ram_slots  # Number of RAM slots
         self.compatibility = compatibility  # List of compatible CPUs, RAM types, etc.
+
+    def is_compatible_with(self, part):
+        """Checks if the given part is compatible with the motherboard."""
+        if isinstance(part, Ram):
+            return part.get_ddr_standard() in self.compatibility
+        elif isinstance(part, Storage):
+            return check_storage_motherboard(part, self)  # Using compatibility manager
+        return True  # Default to True for other parts
 
     def to_string(self):
         base_info = super().to_string()
