45bc1bbb82e3e6619b43ba17a1f131c74d05c4b7
reena-stha <reenashrestha2017@gmail.com>
2025-03-10 21:09:11 -0400

    Basic landing pages and update README.md

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('parts/cpus', views.cpus, name='All CPUs'),
+    path('parts/rams', views.under_construction, name='All RAMs'),
+    path('parts/motherboards', views.under_construction, name='All Motherboards'),
+    path('parts/coolings', views.under_construction, name='All Cooling Accessories'),
+    path('parts/gpus', views.under_construction, name='All GPUs'),
+    path('parts/cases', views.under_construction, name='All Cases'),
+    path('parts/storages', views.under_construction, name='All Storages'),
+    path('parts/accessories', views.under_construction, name='All other accessories'),
 ]
