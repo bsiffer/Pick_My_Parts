b4b8d447e2319d7ac4d543593b29e4d6f6eda54a
reena-stha <reenashrestha2017@gmail.com>
2025-03-22 03:06:55 -0400

    Integrating Django and models

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
-from . import views
+from .views.home import home
+from .views.about import about
+from .views.cpus import cpus
+from .views.under_construction import under_construction
+from .views.cases import cases
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('parts/cpus', views.cpus, name='All CPUs'),
-    path('parts/rams', views.under_construction, name='All RAMs'),
-    path('parts/motherboards', views.under_construction, name='All Motherboards'),
-    path('parts/coolings', views.under_construction, name='All Cooling Accessories'),
-    path('parts/gpus', views.under_construction, name='All GPUs'),
-    path('parts/cases', views.under_construction, name='All Cases'),
-    path('parts/storages', views.under_construction, name='All Storages'),
-    path('parts/accessories', views.under_construction, name='All other accessories'),
+    path('', home, name='home'),
+    path('about', about, name='about'),
+    path('parts/cpus', cpus, name='All CPUs'),
+    path('parts/rams', under_construction, name='All RAMs'),
+    path('parts/motherboards', under_construction, name='All Motherboards'),
+    path('parts/coolings', under_construction, name='All Cooling Accessories'),
+    path('parts/gpus', under_construction, name='All GPUs'),
+    path('parts/cases', cases, name='All Cases'),
+    path('parts/storages', under_construction, name='All Storages'),
+    path('parts/accessories', under_construction, name='All other accessories'),
 ]
