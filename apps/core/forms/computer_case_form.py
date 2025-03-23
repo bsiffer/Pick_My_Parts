725869baeb9687155878e80a8f943c18f6807e23
reena-stha <reenashrestha2017@gmail.com>
2025-03-22 22:15:51 -0400

    Integrating Django and models

--- apps/core/forms/computer_case_form.py
+++ apps/core/forms/computer_case_form.py
@@ -1,16 +1,14 @@
 from django import forms
 from apps.core.models.cases import ComputerCase
+from apps.core.models.form_factor import FormFactor
 
-class ListWidget(forms.TextInput):
-    def value_from_datadict(self, data, files, name):
-        value = data.get(name)
-        if value:
-            return [x.strip() for x in value.strip("[]").split(",")]
-        return []
-    
+
 class ComputerCaseForm(forms.ModelForm):
+    supported_form_factors = forms.ModelMultipleChoiceField(
+        queryset=FormFactor.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
+    
     class Meta:
         model = ComputerCase
         fields = ('manufacturer', 'name', 'sku', 'price', 'form_factor', 'color', 'supported_form_factors')
-        
-    supported_form_factors = forms.CharField(widget=ListWidget(attrs={'placeholder': "[ABC, XYZ]"}))
