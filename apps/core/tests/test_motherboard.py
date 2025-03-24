a32af99ec071965c2b3cbb619b9acb07f5a213d0
anupatrekar <abpatrekar@my.waketech.edu>
2025-03-23 20:57:37 -0400

    Created 2 test classes for motherboard and ram
    Created 2 new test classes in line with the updated test_computer_case.py for interactions with databases and django

---
+++ apps/core/tests/test_motherboard.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from apps.core.models.motherboard import Motherboard
+
+class TestMotherboard(TestCase):
+    def setUp(self):
+        """Set up a sample motherboard for testing."""
+        self.motherboard = Motherboard.objects.create(
+            manufacturer="ASUS",
+            name="ROG Strix B550-F",
+            sku=654321,
+            price=179.99,
+            socket_type="AM4",
+            form_factor="ATX",
+            ram_slots=4,
+            supported_ram_type="DDR4",
+            chipset_compatibility="B550",
+            bios_compatibility="UEFI",
+        )
+
+    def test_motherboard_creation(self):
+        """Test if the motherboard is created with correct attributes."""
+        self.assertEqual(self.motherboard.manufacturer, "ASUS")
+        self.assertEqual(self.motherboard.name, "ROG Strix B550-F")
+        self.assertEqual(self.motherboard.sku, 654321)
+        self.assertEqual(self.motherboard.price, 179.99)
+        self.assertEqual(self.motherboard.socket_type, "AM4")
+        self.assertEqual(self.motherboard.form_factor, "ATX")
+        self.assertEqual(self.motherboard.ram_slots, 4)
+        self.assertEqual(self.motherboard.supported_ram_type, "DDR4")
+        self.assertEqual(self.motherboard.chipset_compatibility, "B550")
+        self.assertEqual(self.motherboard.bios_compatibility, "UEFI")
+
+    def test_string_representation(self):
+        """Test the string rep of the motherboard."""
+        expected_str = (
+            f"{self.motherboard.manufacturer} - {self.motherboard.name} ({self.motherboard.sku})\n"
+            f"Socket Type: {self.motherboard.socket_type}, Form Factor: {self.motherboard.form_factor}, "
+            f"RAM Slots: {self.motherboard.ram_slots}, Supported RAM Type: {self.motherboard.supported_ram_type}"
+        )
+        self.assertEqual(str(self.motherboard), expected_str)
